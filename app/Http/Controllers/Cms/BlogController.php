17b88c2f4262d4528cd3f7de722ba4ea9eb72bea
Md-shefat-masum <mshefat924@gmail.com>
2024-01-17 04:11:19 +0600

    blog cms

---
+++ app/Http/Controllers/Cms/BlogController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class BlogController extends Controller
+{
+
+    public function create()
+    {
+        if(!request()->auth){
+            return redirect('/');
+        }
+        return view('cms.blog.create_blog');
+    }
+
+    public function store()
+    {
+        $json_source = public_path("blogs/web_development.json");
+        $string = file_get_contents($json_source);
+        $contents = json_decode($string, true);
+
+        $temp = [
+            "id" => uniqid().time(),
+            "title" => request()->title,
+            "slug" => Str::slug(request()->title),
+            "date" => request()->date,
+            "writer" => request()->writer,
+            "seo_title" => request()->seo_title,
+            "seo_keywords" => request()->seo_keywords,
+            "seo_description" => request()->seo_description,
+            "image_meta" => request()->image_meta,
+            "image" => "",
+            "description" => request()->description,
+        ];
+
+        if(request()->hasFile('image')){
+            $temp["image"] = Storage::putFileAs('blogs/images',request()->file('image'),Str::slug(request()->title).'.'.request()->file('image')->extension());
+            $temp["image"] .= request()->file('image')->extension();
+        }
+
+        array_unshift($contents, $temp);
+        file_put_contents($json_source, json_encode($contents));
+
+        return redirect()->back();
+    }
+}
