bcfdd5e9f25f08b1aab15054d85314d3d28bc182
Md-shefat-masum <mshefat924@gmail.com>
2024-08-11 01:31:43 +0600

    skill and traning section

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -11,6 +11,8 @@
 @section('contents')
     @include('frontend.pages.home.banner')
     @include('frontend.pages.home.about')
+    @include('frontend.pages.home.skills')
+    @include('frontend.pages.home.training_leadership')
     @include('frontend.pages.home.service')
     @include('frontend.pages.home.portfolio')
     @include('frontend.pages.home.testimonial')
