1f48fa4c9fd3d214dacd8b776cb59bbb8f44c013
Md-shefat-masum <mshefat924@gmail.com>
2024-08-11 01:34:14 +0600

    training and leadership part

---
+++ resources/views/frontend/pages/home/training_leadership.blade.php
@@ -0,0 +1,54 @@
+<!-- Start Blog Section -->
+<section class="cs_100_bg">
+    <div class="cs_height_150 cs_height_lg_75"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 text-center">
+                <div class="cs_section_heading cs_style_1">
+                    <h2 class="cs_section_title cs_font_48 cs_semi_bold">
+                        Mentorship &amp;&nbsp;
+                        <span class="cs_accent_color">
+                            Team Leadership
+                        </span>
+                    </h2>
+                    <p class="cs_section_subtitle cs_accent_color_2 cs_font_16 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
+                        <span>
+                            As a mentor, I am committed to creating a supportive and nurturing environment where team members can grow and develop
+                        </span>
+                        <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M8.3165 0.722028C7.59665 0.85855 7.46009 1.00749 7.3608 1.6529C7.28633 2.17419 7.27388 2.1866 7.01329 2.02525C5.33769 0.933026 1.46525 3.71325 2.66918 5.14059C3.30217 5.88533 3.48833 6.25767 3.57521 6.38178C1.5521 6.69201 0.96875 6.64236 0.96875 8.90136C0.96875 10.7755 1.46522 10.6762 3.82344 10.8376C3.61244 11.1355 2.7809 12.4387 2.30925 12.9848C1.8252 13.5433 1.84999 13.6674 2.59469 14.4494C4.76674 16.7455 5.90861 17.1179 7.05048 15.9015C7.5221 15.4051 7.6463 15.492 7.65866 16.3856C7.68349 17.4778 8.94951 17.7881 12.1889 17.4654C14.2245 17.2668 14.0382 17.031 13.8769 15.8643L15.1802 16.4104C16.5082 16.9689 17.0171 16.5966 19.636 13.1337C19.7973 12.9227 19.3133 12.5008 18.9161 12.5008C18.7671 12.5008 18.3699 12.2525 18.0348 11.9546L17.4142 11.4085C17.8487 11.3589 18.5685 11.2596 19.1519 11.1603C19.4497 11.1106 19.6608 10.8624 19.6856 10.5645C19.8097 8.78962 19.8594 8.0325 19.8594 8.04495C19.8718 7.84638 19.7725 7.64771 19.6111 7.53605C19.4249 7.41194 19.1147 7.25055 18.5686 7.1389C17.6997 6.96514 17.6873 7.05198 18.6678 6.00944C19.7228 4.86753 19.7477 4.942 17.8735 3.01819C16.26 1.35502 14.9692 0.833728 14.1748 1.49155L13.7901 1.81425L13.7155 1.50396C13.5294 0.759261 10.4513 0.312435 8.3165 0.722028ZM11.7918 3.30365C11.9034 5.94734 12.3627 6.17074 13.9266 4.35865C15.5526 2.48449 15.3044 2.55895 16.409 3.61395C17.5881 4.731 17.6253 4.55724 15.9993 6.08391C14.1624 7.79673 14.2866 8.15661 16.6199 7.90839C18.3327 7.72218 18.1465 8.10696 17.9728 10.2542C14.8698 10.4528 13.8273 10.4528 15.4656 11.6195C16.744 12.538 17.3149 13.084 17.166 13.2826C17.0915 13.3695 15.5898 15.2313 15.5898 15.2313C15.3912 15.0824 15.0933 14.9583 14.0755 13.9777C12.5861 12.5628 12.0897 12.7738 12.1393 14.8465L12.1766 16.3236C9.64453 16.6711 9.73145 16.8324 9.59489 14.5238C9.54524 13.6799 9.65699 12.1656 7.94417 12.8358C7.17459 13.1337 6.20649 13.9156 5.80931 14.6479C5.63555 14.9706 5.51145 14.921 4.70469 14.3004C3.81104 13.6178 3.78622 13.8163 4.89086 12.5131C5.25079 12.0912 5.61075 11.6071 5.83416 11.2719C6.70298 9.93153 5.39977 9.64603 3.81108 9.64603H3.09116C3.09116 9.64603 3.10358 8.2559 3.21528 7.63534C3.3394 7.63534 4.13374 7.64771 4.89086 7.66017C6.92637 7.68499 6.98845 7.63534 6.08239 6.64236C5.22599 5.71149 4.7419 5.09094 4.44403 4.74341C4.61779 4.58206 3.83589 5.33917 6.09482 3.1423C6.23134 3.29124 6.93879 4.03594 7.62148 4.85511C8.4778 5.88524 9.48323 5.50053 9.48323 4.80547V4.06077C9.4956 1.40467 9.38385 1.59084 10.9106 1.62807L11.7173 1.64049L11.7918 3.30365Z"
+                                fill="#342EAD"
+                            ></path>
+                        </svg>
+                    </p>
+
+                </div>
+            </div>
+
+            <div class="col-12 mt-4">
+                <div>
+                    <h5>
+                        Training Experience
+                    </h5>
+                    <p class="text-justify">
+                        Since 2018, I have been dedicated to training aspiring full stack developers, with over 400 students taught to date. My training approach emphasizes practical, hands-on learning, ensuring that students not only understand the theoretical aspects of development but also gain the skills needed to build real-world applications.
+                        I'm proud to have mentored more than 20 of my students who have gone on to secure positions in various IT fields, contributing to the tech industry with the skills and knowledge they acquired under my guidance.
+                    </p>
+                </div>
+                <div>
+                    <h5>
+                        Team Leadership
+                    </h5>
+                    <p class="text-justify">
+                        In addition to my role as a trainer, I have been working as a team leader since 2022, guiding development teams through complex projects from conception to deployment. My leadership style focuses on fostering collaboration, encouraging continuous learning, and ensuring that every team member contributes to the project's success. I excel at project management, task distribution, and problem-solving, which enables my teams to deliver high-quality software solutions on time and within scope.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="cs_height_150 cs_height_lg_80"></div>
+    </div>
+</section>
+<!-- End Blog Section -->
